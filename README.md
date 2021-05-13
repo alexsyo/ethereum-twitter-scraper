cc8d2d231549e369cbfc5658c82f9c593aaebd3c
alexs <alex_ok@hotmail.it>
2021-05-13 22:43:24 +0200

    twitter scraper

---
+++ README.md
@@ -0,0 +1,37 @@
+# Twitter scraper (for Ethereum addresses)
+
+
+
+## Prerequisite
+
+latest version of node installed on the system
+
+## Installation
+
+Run the following commands in the project directory:
+
+install yarn
+
+```
+npm i -g yarn
+```
+
+install dependencies
+
+```
+yarn
+```
+
+## Setup
+
+Rename `.env.example` to `.env` and substitute the variables
+
+## Run
+
+```
+node index.js
+```
+
+A file called data.csv will appear if successful
+
+results are sorted by date of creation
---
+++ index.js
@@ -0,0 +1,93 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const { ethers } = require('ethers')
+const provider = new ethers.providers.CloudflareProvider()
+require('dotenv').config()
+
+
+const uri = process.env.URI;
+const tokenAddress = process.env.TOKEN_ADDRESS;
+const tokenId = process.env.TOKEN_ID;
+const quantity = process.env.QUANTITY;
+
+
+
+(async () => {
+  // start browser
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--window-size=600,800'],
+    defaultViewport: null
+  });
+  const page = await browser.newPage();
+  await page.goto(uri, { waitUntil: 'networkidle0' });
+  await page.screenshot({ path: 'initial-load.png' });
+  
+
+  const items = [] 
+  let currentHeight = 0;
+  let scrollHeight = 1000
+
+  // scroll and grab addresses
+  while(currentHeight < scrollHeight) {
+    const { data, scroll } = await page.evaluate(async () => {
+      // scrape replies
+      const replies = Array.from(document.querySelectorAll('article div[data-testid=tweet] > div:nth-of-type(2)'))
+      window.scrollBy(0, 30);
+
+      // click show more if exists
+      const buttons = Array.from(document.querySelectorAll('span')).filter(x => /Show/.test(x.innerText))
+      buttons.forEach(b => b.click())
+
+      return {
+        data: replies.map(r => r.innerHTML),
+        scroll: document.body.scrollHeight,
+      }
+    });
+
+    scrollHeight = scroll;
+    currentHeight += 30;
+
+    // added address to items
+    for(let i = 0; i < data.length; i++) {
+      const d = data[i]
+      const address = d.match(/0x[a-fA-F0-9]{40}|[a-zA-Z0-9]*\.eth/g)
+      const time = d.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}Z/g)
+      if(address && address[0] && time && time[0] && !items.find(x => x.address === address[0] || x.ens === address[0])) {
+
+        if(/\.eth/.test(address[0])) {
+          const resolvedAddress = await provider.resolveName(address[0]);
+          console.log(`resolved ${address[0]} to ${resolvedAddress}`);
+          items.push({
+            address: resolvedAddress,
+            ens: address[0],
+            time: time[0],
+          });
+        } else {
+          items.push({
+            address: address[0],
+            time: time[0]
+          })
+        }
+
+      }
+    }
+  }
+  
+
+  // write to csv
+  let string = '"Recipient","Token Address","Token Id","Quantity"\n'
+  items
+    .sort((a,b) => {
+      return new Date(a.time) - new Date(b.time) 
+    })
+    .forEach(i => {
+      string += `"${i.address}","${tokenAddress}","${tokenId}","${quantity}"\n`
+    })
+
+  fs.writeFileSync('./data.csv', string);
+
+
+
+  await browser.close();
+})();
